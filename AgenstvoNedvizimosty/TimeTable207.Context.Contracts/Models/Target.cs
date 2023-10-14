eebe1fc1ec10cdd8063745ba78b7d55fe8fa771d
Gordan23411 <104491607+Gordan23411@users.noreply.github.com>
2023-10-14 12:17:26 +0300

    Добавлены классы и строки данных

---
+++ AgenstvoNedvizimosty/TimeTable207.Context.Contracts/Models/Target.cs
@@ -0,0 +1,21 @@
+﻿namespace TimeTable207.Context.Contracts.Models
+{
+    /// <summary>
+    /// Инфа о путевке
+    /// </summary>
+    internal class Target : BaseAuditEntity
+    {
+        /// <summary>
+        /// название путевки
+        /// </summary>
+        public decimal namePut { get; set; }
+        /// <summary>
+        /// куда люди полетят 
+        /// </summary>
+        public string KudaAdress { get; set; }
+        /// <summary>
+        /// цена за 1-го человека
+        /// </summary>
+        public decimal PriseHuman { get; set; } 
+    }
+}
