18cb2bd9cda1de9fb330f2ac1513336910198b49
Gordan23411 <104491607+Gordan23411@users.noreply.github.com>
2023-10-28 12:02:51 +0300

    Живой контроллер

---
+++ AgenstvoNedvizimosty/AgenstvoNedvizimosty/Controllers/TuristController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TimeTable207.Context;
+using TimeTable207.Context.Contracts;
+
+namespace AgenstvoNedvizimosty.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class TuristController : ControllerBase
+    {
+        [HttpGet] // localhost:1111224/group
+        public IActionResult Turist()
+        {
+            var context = new TimeTableContext() as ITimeTableContext;
+            var turists = context.Turists.ToList();
+            return Ok(turists);
+        }
+    }
+}
